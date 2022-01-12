6c9f5ab58c15c987bdfccd7f8c5c974aa882af71
chris-y <maxsummer256@gmail.com>
2022-01-13 00:54:43 +0900

    fix: submitMovingInfo in tasks module - 필요한 정보만 response하도록 spl query 수정

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Get, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserJwtAuthGuard } from 'src/auth/user-jwt-auth.guard';
 import { GetMyInfo } from 'src/common/decorator/get-myInfo.decorator';
-import { Users } from 'src/entities/Users';
 import { UserWithoutPasswordDto } from 'src/users/dto/user-without-password.dto';
 import { TasksService } from './tasks.service';
 
@@ -15,16 +14,15 @@ export class TasksController {
 
     // 내 이사정보 모두 가져오기 api
 
-    @ApiOperation({ summary: '견적을 받기 위한 이사정보 발송'})
+    @ApiOperation({ summary: '견적을 받기 위해 기사님들에게 이사정보 발송'})
     @ApiResponse({ status: 200, description: 'response 성공' })
     @ApiResponse({ status: 401, description: 'response 실패' })
     @ApiBearerAuth('JWT-Auth')
     @UseGuards(UserJwtAuthGuard)
     @Get('submit/movingInfo')
     submitMovingInfo(
-        @GetMyInfo() user: Users,
+        @GetMyInfo() user: UserWithoutPasswordDto,
         ) {
-        console.log('user:::::::',user)
         return this.taskService.submitMovingInfo(user.id)
     }
 
